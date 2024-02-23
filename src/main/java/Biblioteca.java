e166f3628d0bbaba7138aa60aec6e2d40318a811
Dayana Haro <harocarolina24@gmail.com>
2024-02-23 12:15:04 -0500

    MVC

---
+++ src/main/java/Biblioteca.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Biblioteca {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
